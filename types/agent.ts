3ea58553e6de2f81c26d640df620c597f9c776d9
smazik02 <s.mazik2002@gmail.com>
2025-12-09 22:49:00 +0100

    updated frontend<->backend DTOs

--- types/agent.ts
+++ types/agent.ts
@@ -15,3 +15,25 @@ interface AgentMG {
     headscale_id: string | null;
     auth_key: string | null;
 }
+
+// DTOs
+interface ReturnAgentDTO {
+    id: string; // UUID
+    name: string;
+    hostname: string | null;
+    isRegistered: boolean;
+    isConnected: boolean;
+    workspaceId: string; // UUID
+    workspaceName: string;
+    authKey: string | null;
+}
+
+interface CreateAgentDTO {
+    name: string;
+    workspaceId: string; // UUID
+}
+
+interface UpdateAgentDTO {
+    name: string | null;
+    // there are more, but realistically frontend will only update this
+}
--- types/workspace.ts
+++ types/workspace.ts
@@ -1,27 +1,33 @@
 interface WorkspacePG {
-	id: string; // PK, UUID
-	name: string;
+    id: string; // PK, UUID
+    name: string;
 
-	user_id: string; // FK
+    user_id: string; // FK
 }
 
 interface WorkspaceMG {
-	name: string;
-	conf_id: string;
-	pg_id: string; // "FK" from PG
-	vm_ip_priv_addr: string;
-	vm_azure_domain: string;
-	vm_ssh_key: string;
+    name: string;
+    conf_id: string;
+    pg_id: string; // "FK" from PG
+    vm_ip_priv_addr: string;
+    vm_azure_domain: string;
+    vm_ssh_key: string;
 }
 
 // DTOs
-interface CreateWorkspaceDto {
-	user_id: string;
-	workspace_id: string;
-	workspace_name: string;
+interface ReturnWorkspaceDTO {
+    id: string; // UUID
+    name: string;
+    userId: string; // UUID
+    agentCount: number;
+    azureDomain: string | null;
+    publicIP: string | null;
 }
 
-interface CreateAgentDto {
-	workspace_id: string;
-	name: string;
+interface CreateWorkspaceDTO {
+    name: string;
+}
+
+interface UpdateWorkspaceDTO {
+    name: string | null;
 }
